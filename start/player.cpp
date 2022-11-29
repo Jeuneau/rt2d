d8a5f9c8992938379a452c23b194df6ae2a69c06
Jeuneau <boekhoudtjeuneau@gmail.com>
2022-11-29 15:24:11 +0100

    player start

---
+++ start/player.cpp
@@ -0,0 +1,24 @@
+#include <rt2d/core.h>
+#include "myscene.h"
+
+class player {
+
+	int health;
+
+	list<bullets>
+
+	Player() {
+	}
+	BulletShoot() {
+
+	}
+
+	void Move() {
+
+	}
+}
+
+
+int main(void) {
+
+}
