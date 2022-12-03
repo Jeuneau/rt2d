104a08f874de3bf28c4c95446e0e0d5032d7fa77
Jeuneau <boekhoudtjeuneau@gmail.com>
2022-12-03 21:26:24 +0100

    spaceship

--- start/myscene.cpp
+++ start/myscene.cpp
@@ -9,6 +9,15 @@
 
 #include "myscene.h"
 
+
+
+
+
+
+
+
+
+
 MyScene::MyScene() : Scene()
 {
 	// start the timer.
@@ -16,8 +25,14 @@ MyScene::MyScene() : Scene()
 
 	// create a single instance of MyEntity in the middle of the screen.
 	// the Sprite is added in Constructor of MyEntity.
+
+	
+	
+	// delete when you're done with it.
 	myentity = new MyEntity();
-	myentity->position = Point2(SWIDTH/2, SHEIGHT/2);
+	myentity->position = Point2(SWIDTH/2, SHEIGHT/2); 
+
+
 
 	// create the scene 'tree'
 	// add myentity to this Scene as a child.
@@ -32,10 +47,78 @@ MyScene::~MyScene()
 
 	// delete myentity from the heap (there was a 'new' in the constructor)
 	delete myentity;
+
+	
 }
+
+class player {
+
+	int health;
+	health = 0;
+
+	if (health <= 0) {
+		Destroy Player;
+
+	}
+
+	else {
+		Player.isAlive
+	}
+
+
+
+
+	list<bullets>
+
+		Player() {
+	}
+	BulletShoot() {
+
+	}
+
+	void Move() {
+
+	}
+}
+
+
+int main(void) {
+
+}
+
+void Scene03::updateSpaceShip(float deltaTime)
+{
+	spaceship->line()->color = WHITE;
+
+	float rotspeed = 3.14f;
+
+	static Vector2 velocity = Vector2((rand() % 100) - 50, (rand() % 100) - 50);
+	static Polar polar = Polar((rand() % 360) * DEG_TO_RAD, 400.0f);
+
+	if (input()->getKey(KeyCode::Up)) {
+		spaceship->line()->color = RED;
+		velocity += polar.cartesian() * deltaTime; // thrust
+	}
+	if (input()->getKey(KeyCode::Right)) {
+		polar.angle += rotspeed * deltaTime; // rotate right
+	}
+	if (input()->getKey(KeyCode::Left)) {
+		polar.angle -= rotspeed * deltaTime; // rotate left
+	}
+
+	spaceship->rotation.z = polar.angle;
+	spaceship->position += velocity * deltaTime;
+
+	if (spaceship->position.x < 0) { spaceship->position.x = SWIDTH; }
+	if (spaceship->position.x > SWIDTH) { spaceship->position.x = 0; }
+	if (spaceship->position.y < 0) { spaceship->position.y = SHEIGHT; }
+	if (spaceship->position.y > SHEIGHT) { spaceship->position.y = 0; }
 
 void MyScene::update(float deltaTime)
 {
+	
+
+	
 	// ###############################################################
 	// Escape key stops the Scene
 	// ###############################################################
@@ -61,4 +144,6 @@ void MyScene::update(float deltaTime)
 		myentity->sprite()->color = Color::rotate(color, 0.01f);
 		t.start();
 	}
-}
+
+	
+} 
--- start/player.cpp
+++ start/player.cpp
@@ -1,37 +1,7 @@
 #include <rt2d/core.h>
 #include "myscene.h"
 
-class player {
-
-	int health;
-	health = 0;
-
-	if (health <= 0) {
-		Destroy Player;
-
-	}
-
-	else {
-		Player.isAlive
-	}
-	
 
 
 
-	list<bullets>
 
-	Player() {
-	}
-	BulletShoot() {
-
-	}
-
-	void Move() {
-
-	}
-}
-
-
-int main(void) {
-
-}
