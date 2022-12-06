ffb645f44170bcf9d8b9a05f1feea41c80c2776b
Jeuneau <boekhoudtjeuneau@gmail.com>
2022-12-06 09:33:07 +0100

    .

--- start/health.cpp
+++ start/health.cpp
@@ -4,14 +4,14 @@ Health::Health() {
 	health = 10;
 
 	// draw the text
-	setPlainText("Health: ") + Health::number(health); // Health: 3
+	/*setPlainText("Health: ") + Health::number(health); // Health: 3
 	setDefaultTextColor(red);
-	setFont("times", 16);
+	setFont("times", 16); */
 }
 
 void Health::decrease() {
 	health--;
-	setPlainText("Health: ") + number(health); // Health: 2
+	/*setPlainText("Health: ") + number(health); // Health: 2*/
 }
 
 int Health::getHealth() {
