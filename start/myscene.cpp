e0fa435c070b899ce38d8f63cd97e72904abf515
Jeuneau <boekhoudtjeuneau@gmail.com>
2022-12-08 09:48:44 +0100

    .

--- start/myscene.cpp
+++ start/myscene.cpp
@@ -36,11 +36,9 @@ MyScene::MyScene() : Scene()
 	// add myentity to this Scene as a child.
 	this->addChild(myentity);
 
-	//bullet list
+	
 
-		vector<string> bullets;
-		bullets.push_back("Bullet1");
-		bullets.push_back("Bullet2");
+		
 
 		//return 0;
 }
@@ -93,3 +91,62 @@ void MyScene::update(float deltaTime)
 
 	
 } 
+
+/*struct Bullet
+{
+	int x = rand() % 100;
+	int y = rand() % 100;
+};
+
+int main(void)
+{
+	std::cout << "Bullet Test" << std::endl;
+
+	// an empty container on the stack that contains pointers to actual Bullet instances on the heap
+	std::vector<Bullet*> bullets;
+
+	// #############################################################
+	// make some new bullets (or add a single Bullet when the fire-key is pressed)
+	std::cout << "================== current 10 bullets ==================" << std::endl;
+	for (int i = 0; i < 10; i++) {
+		Bullet* b = new Bullet(); // create a new Bullet on the heap
+		bullets.push_back(b); // keep a pointer to that Bullet in our list
+		// addChild(bullets[i]); // if applicable
+
+		std::cout << i << " : (" << b->x << "," << b->y << ")";
+		if (b->y > 50) {
+			std::cout << " <-- need to delete this bullet (y>50)";
+		}
+		std::cout << std::endl;
+	}
+
+	// #############################################################
+	// loop backwards through bullets, to delete them from the heap and from the list.
+	std::cout << "================== delete bullets if y>50 ==================" << std::endl;
+	for (int i = bullets.size() - 1; i >= 0; i--) { // backwards!!!
+		if (bullets[i]->y > 50) {
+			std::cout << "-- deleting Bullet " << i << " : (" << bullets[i]->x << "," << bullets[i]->y << ")" << std::endl;
+			// removeChild(bullets[i]); // if applicable
+			delete bullets[i]; // delete from the heap first
+			bullets.erase(bullets.begin() + i); // then, remove from the list
+		}
+	}
+
+	// #############################################################
+	std::cout << "================== our current list of bullets ==================" << std::endl;
+	for (int i = 0; i < bullets.size(); i++) {
+		std::cout << i << " : (" << bullets[i]->x << "," << bullets[i]->y << ")" << std::endl;
+	}
+
+	// #############################################################
+	// at the very end (close program / delete scene), delete all bullets still in the list
+	std::cout << "================== delete the rest of the bullets ==================" << std::endl;
+	for (int i = 0; i < bullets.size(); i++) {
+		std::cout << "-- deleting Bullet " << i << " : (" << bullets[i]->x << "," << bullets[i]->y << ")" << std::endl;
+		delete bullets[i]; // delete Bullet from the heap (a pointer to it is still in the list)
+		bullets[i] = nullptr; // set Bullet pointer to nullptr (don't try to remove it from the list)
+	}
+	bullets.clear(); // list contains only NULL pointers. Empty the list with 1 command.
+
+	return 0;
+} */
